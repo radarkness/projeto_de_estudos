1ebb890a765055fb6dac31bbe10db79182008d4e
Rafael Pinheiro <82719410+radarkness@users.noreply.github.com>
2024-08-19 10:05:59 -0300

    19/08

---
+++ Python/Aulas Phyton/Faculdade_desenvolvimento_Back-End/Aula02.py
@@ -0,0 +1,15 @@
+
+#for i in range(9990, 10000, 1):
+#    print("Oláááááá...")
+
+#i = 0
+#while i < 1000000:
+#    print("Olá Senhorita Atum")
+#    i = i + 100000
+
+idades = [34, 27, 44, 93, 22]
+
+idades[3] = 29
+
+for i in idades:
+    print(i)
---
+++ Python/Aulas Phyton/Faculdade_desenvolvimento_Back-End/ExAul02.py
@@ -0,0 +1,13 @@
+notas = [6,7,6.5,4.8,8]
+soma = 0
+for i in range(5):
+    soma = soma + notas[i]
+media = soma/5
+print("Média: %.2f" %media)
+
+
+#nomes = ["Maria", "Pricila", "Fernanda"]
+#print(len(nomes))
+
+#for i in range(10):
+#    print(i, end="")
